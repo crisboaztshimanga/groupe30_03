a69f40d93875686f19817694b9aa3778d0eb7e08
crisboaztshimanga <crisboaztshimangakabanza@gmail.com>
2025-07-15 14:45:20 +0100

    01_exercice01.py

---
+++ 01_variables_&_type_de_donnée /01_exercice01.py
@@ -0,0 +1,20 @@
+# Nom : TSHIMANGA 
+# Post-nom : KABANZA
+# Prénom : Cris_Boaz
+# groupe : 30
+
+# Demande d'informations à l'utilisateur
+prenom = input("Entrez votre prénom : ")
+age = int(input("Entrez votre âge : "))
+ville = input("Entrez votre ville : ")
+metier = input("Entrez votre métier : ")
+
+# Approximation des jours vécus
+jours_vecus = age * 365
+
+# Affichage formaté
+print("\n== PROFIL UTILISATEUR ==")
+print(f"Prénom : {prenom}")
+print(f"Age : {age} ans ({jours_vecus} jours vécus environ)")
+print(f"Ville : {ville}")
+print(f"Métier : {metier}")
